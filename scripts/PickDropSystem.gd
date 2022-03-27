1871ae13312c147617acf1d50904962edac2600e
Airaldi <sergioairaldi@gmail.com>
2022-03-28 00:38:30 +0200

    Adding first part of  PickDrop System to the project

---
+++ scripts/PickDropSystem.gd
@@ -0,0 +1,15 @@
+class_name PickDropSystem
+
+var owner
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func pick():
+	return self
+
+
+func drop():
+	owner.global_transform
