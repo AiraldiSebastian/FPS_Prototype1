b811bd45c4d7ca4aa8667c399976286c5be1dc0a
Airaldi <sergioairaldi@gmail.com>
2022-12-24 04:03:56 +0100

    Small fix in Zombie.gd

--- scenes/Units/Enemies/Zombie.gd
+++ scenes/Units/Enemies/Zombie.gd
@@ -48,10 +48,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	#if bodyInAttackArea:
-	
-	if bodyInAttackArea:
-		look_at(bodyInAttackArea.global_transform.origin, Vector3(0, 1, 0))
+	if bodyInAttackArea or zombieAnim.get_current_animation() == "attack":
+		#look_at(bodyInAttackArea.global_transform.origin, Vector3(0, 1, 0))
 		if zombieAnim.get_current_animation() != "attack":
 			zombieAnim.play("attack");
 	elif bodyInDetectionArea:
